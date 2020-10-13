7c6511c8d1bbe5cfd90e1e2ab6586ff7286e37a8
Frank de Jonge <info@frenky.net>
2020-10-13 09:23:44 +0200

    Make email and name optional

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ import * as os from 'os';
 (async () => {
     const token = core.getInput('token', { required: true });
     const credentials = `https://${token}@github.com/`;
-    const userName = core.getInput('user_name', { required: true });
-    const userEmail = core.getInput('user_email', { required: true });
+    const userName = core.getInput('user_name', { required: false }) || '';
+    const userEmail = core.getInput('user_email', { required: false }) || '';
 
     let configDir = process.env['XDG_CONFIG_HOME'] || `${os.homedir()}/.config`;
     let gitConfigDir = `${configDir}/git`;
@@ -16,8 +16,15 @@ import * as os from 'os';
 
     await fs.mkdir(gitConfigDir, { recursive: true });
     await fs.writeFile(gitCredentialsFile, credentials, { flag: 'a', mode: 0o600 });
-    // await exec('git', ['config', '--global', 'user.name', userName]);
-    // await exec('git', ['config', '--global', 'user.email', userEmail]);
+
+    if (userName.length > 0) {
+        await exec('git', ['config', '--global', 'user.name', userName]);
+    }
+
+    if (userEmail.length > 0) {
+        await exec('git', ['config', '--global', 'user.email', userEmail]);
+    }
+
     await exec('git', ['config', '--global', 'credential.helper', 'store']);
     await exec('git', ['config', '--global', '--replace-all', 'url.https://github.com/.insteadOf', 'ssh://git@github.com/']);
     await exec('git', ['config', '--global', '--add', 'url.https://github.com/.insteadOf', 'git@github.com:']);
